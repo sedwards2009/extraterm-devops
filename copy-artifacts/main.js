5c5740499f2e1014c8cb8bbed118e1c00e94034c
Simon Edwards <simon@simonzone.com>
2019-04-02 22:54:43 +0200

    Try to get more info about the environment.

--- copy-artifacts/main.js
+++ copy-artifacts/main.js
@@ -2,6 +2,18 @@
 // https://github.com/sedwards2009/extraterm-devops
 // License: MIT
 
-// const storage = require("@google-cloud/storage")({projectId: "extraterm"});
+const log = console.log.bind(console);
+
+const fs = require("fs");
+
+// const storage = require("@google-cloud/storage")({
+//   projectId: "extraterm",
+//   keyFilename: "extraterm-e7f88c1ce73f.json"
+// });
+
 // const bucket = storage.bucket("extraterm_builds");
-console.log("Helloooo!");
+
+
+for (const entry of fs.readdirSync("../extraterm.artifacts")) {
+  log(entry);
+}
