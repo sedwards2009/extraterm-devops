c220acbfadf4e2fae617e7633b5ccfae381c5a63
Simon Edwards <simon@simonzone.com>
2019-04-02 23:22:07 +0200

    Add the real upload parts.

--- copy-artifacts/main.js
+++ copy-artifacts/main.js
@@ -4,16 +4,34 @@
 
 const log = console.log.bind(console);
 
+const dayjs = require("dayjs");
 const fs = require("fs");
+const path = require("path");
 
-// const storage = require("@google-cloud/storage")({
-//   projectId: "extraterm",
-//   keyFilename: "extraterm-e7f88c1ce73f.json"
-// });
+const ARTIFACT_PATH = "../../extraterm.artifacts";
 
-// const bucket = storage.bucket("extraterm_builds");
+function* main() {
+  const storage = require("@google-cloud/storage")({
+    projectId: "extraterm",
+    keyFilename: "extraterm-e7f88c1ce73f.json"
+  });
 
+  const bucket = storage.bucket("extraterm_builds");
 
-for (const entry of fs.readdirSync("../../extraterm.artifacts")) {
-  log(entry);
+  const dirBase = dayjs().format("YYYY-MM-DD HH:mm:ss");
+  log(dirBase);
+
+  for (const entry of fs.readdirSync(ARTIFACT_PATH)) {
+    const destinationPath = dirBase + "/" + entry;
+    log(`Uploading ${entry} to ${destinationPath}`);
+
+    await bucket.upload(path.join(ARTIFACT_PATH, entry), {
+      destination: destinationPath,
+      resumable: true,
+      validation: "crc32c",
+    });
+  }
+  log("Done");
 }
+
+main();
