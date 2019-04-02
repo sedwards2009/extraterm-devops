43e388b1c53fc567b4a2748db43cc05777db7eba
Simon Edwards <simon@simonzone.com>
2019-04-02 22:14:48 +0200

    Initial artifact copy script.

---
+++ copy-artifacts/main.js
@@ -0,0 +1,7 @@
+// Simon Edwards <simon@simonzone.com>
+// https://github.com/sedwards2009/extraterm-devops
+// License: MIT
+
+// const storage = require("@google-cloud/storage")({projectId: "extraterm"});
+// const bucket = storage.bucket("extraterm_builds");
+console.log("Helloooo!");
